fb52abe0cac60c5b45120086a5de0874adabbc3e
NewtonCZ <niutoncaraballo3@gmail.com>
2026-09-21 20:50:48 -0400

    Update ExpressView.tsx

--- src/components/player/ExpressView.tsx
+++ src/components/player/ExpressView.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { useGame } from '../../context/GameContext';
 import { getFichaById } from '../../data/fichasPool';
-import { FichaBadge } from '../cards/FichaBadge';
+import { FichaBadge } from '../common/FichaBadge';
 import { Sparkles, Zap, Trophy, AlertCircle, RotateCcw, Play } from 'lucide-react';
 
 interface PlayResult {
